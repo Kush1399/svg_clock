e8448a391c7227946829c5abd609cc22db320d28
Kush1399 <84185904+Kush1399@users.noreply.github.com>
2022-01-31 17:53:59 +0530

    Add files via upload

--- clock.js
+++ clock.js
@@ -3,6 +3,9 @@ const minuteHand = document.getElementById("minute-hand")
 const secondHand = document.getElementById("second-hand")
 const dayText = document.getElementById("date")
 const timeAMPM = document.getElementById("time")
+const monthText = document.getElementById("month")
+
+const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEPT", "OCT", "NOV", "DEC"]
 
 const animate = () => {
   const date = new Date()
@@ -12,12 +15,14 @@ const animate = () => {
   const sec = date.getSeconds() + date.getMilliseconds() / 1000
   const day = date.getDate()
   const ampm = date.getHours() >= 12 ? "PM" : "AM"
+  const month = months[date.getMonth()]
 
   hourHand.setAttribute("transform", `rotate(${(360 / 12) * hour})`)
   minuteHand.setAttribute("transform", `rotate(${(360 / 60) * min})`)
   secondHand.setAttribute("transform", `rotate(${(360 / 60) * sec})`)
   dayText.textContent = day
   timeAMPM.textContent = ampm
+  monthText.textContent = month
 
   requestAnimationFrame(animate)
 }
